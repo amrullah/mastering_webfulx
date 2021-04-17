c668134a2a3c1256c337d41ee0fe57ac962f7a7b
amrullah <amrullah.zunzunia@gmail.com>
2021-04-17 23:07:02 +0530

    Flux error handling

---
+++ learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoErrorTest.java
@@ -0,0 +1,70 @@
+package com.learnreactivespring.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+
+public class FluxAndMonoErrorTest {
+    @Test
+    public void fluxErrorHandling_OnErrorResume() {
+        Flux<String> stringFlux = Flux.just("A", "B", "C")
+                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .concatWith(Flux.just("D"))
+                .onErrorResume((e) -> {  // on error, subscribe to a fallback publisher
+                    System.out.println("Exception is: " + e);
+                    return Flux.just("default", "default1");
+                });
+
+        StepVerifier.create(stringFlux.log())
+                .expectSubscription()
+                .expectNext("A", "B", "C")
+                .expectNext("default", "default1")
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxErrorHandling_OnErrorReturn() {
+        Flux<String> stringFlux = Flux.just("A", "B", "C")
+                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .concatWith(Flux.just("D"))
+                .onErrorReturn("default");  // On error, just return a fallback value
+
+        StepVerifier.create(stringFlux.log())
+                .expectSubscription()
+                .expectNext("A", "B", "C")
+                .expectNext("default")
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxErrorHandling_OnErrorMap() {
+        Flux<String> stringFlux = Flux.just("A", "B", "C")
+                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .concatWith(Flux.just("D"))
+                .onErrorMap(CustomException::new);  // On error, transform the exception
+
+        StepVerifier.create(stringFlux.log())
+                .expectSubscription()
+                .expectNext("A", "B", "C")
+                .expectError(CustomException.class)
+                .verify();
+    }
+
+    @Test
+    public void fluxErrorHandling_OnErrorMap_WithRetry() {
+        Flux<String> stringFlux = Flux.just("A", "B", "C")
+                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .concatWith(Flux.just("D"))
+                .onErrorMap(CustomException::new)
+                .retry(2);  // On error, retry the whole flux, max upto 2 times
+
+        StepVerifier.create(stringFlux.log())
+                .expectSubscription()
+                .expectNext("A", "B", "C")
+                .expectNext("A", "B", "C")  // flux replayed two more times.
+                .expectNext("A", "B", "C")
+                .expectError(CustomException.class)
+                .verify();
+    }
+}
